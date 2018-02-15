226c68909f1516df791210a34889c596723eee5e
IsaacAboytes <isaac.aboytes@gmail.com>
2018-02-14 21:00:53 -0600

    Git inicial de un proyecto de Android studio

--- app/src/main/java/sabado/sem7/android/diplo/gitandroid/MainActivity.java
+++ app/src/main/java/sabado/sem7/android/diplo/gitandroid/MainActivity.java
@@ -2,12 +2,34 @@ package sabado.sem7.android.diplo.gitandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private Button miBoton;
+    private TextView txtVista;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        miBoton=findViewById(R.id.boton);
+        txtVista=findViewById(R.id.txtSaludo);
+
+        miBoton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                txtVista.setText("Kalimera Git");
+                Toast.makeText(MainActivity.this, "Se camnbió" +
+                        "el mensaje del textView", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
